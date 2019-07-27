a5fe6cea1a430c5e39812b17f947e3c76a227225
Harsh Sahu <harsh.sahu@outlook.com>
2019-07-27 18:06:45 +0530

    token

--- project.py
+++ project.py
@@ -401,5 +401,7 @@ def getUserID(email):
 
 
 if __name__ == '__main__':
+    #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
     app.debug = True
     app.run()
